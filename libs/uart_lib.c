9d374bf506bf7274872051e2fde50bd4ec19e1ec
Sergey Shchelkanov <shchelkanov-s@yandex.ru>
2023-03-09 10:50:57 +0300

    xputc added in uart

--- libs/uart_lib.c
+++ libs/uart_lib.c
@@ -116,3 +116,9 @@ void UART_ClearRxFifo(UART_TypeDef* uart)
         (void)uart->RXDATA;
     }
 }
+
+void __attribute__((weak)) xputc(char c)
+{
+	UART_WriteByte(UART_0, c);
+	UART_WaitTransmission(UART_0);
+}
